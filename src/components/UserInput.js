484d51d2c75937e2c966575ad8c63b1c20a5674a
trebordng <96870545+trebordng@users.noreply.github.com>
2022-07-29 00:20:18 +1000

    change validation

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import { Grid,TextField} from '@mui/material'
-import '../App.css'
+import React from "react";
+import { Grid, TextField } from "@mui/material";
+import "../App.css";
 
 const UserInput = (props) => {
-const handleChange = event =>{
-    props.setNum(parseInt(event.target.value))
-   
-}
+  const handleChange = (event) => {
+    if (parseInt(event.target.value) < 0) {
+      alert("Please put in a positive number!");
+    } else {
+      props.setNum(parseFloat(event.target.value));
+    }
+  };
   return (
     <Grid container item xs={6} md={3}>
-        <Grid xs={12} className="input-name">{props.name}</Grid>
-        <Grid xs={12}> <TextField className='input-field' type="number" value={props.num} onChange={handleChange}/></Grid>
+      <Grid xs={12} className="input-name">
+        {props.name}
+      </Grid>
+      <Grid xs={12}>
+        {" "}
+        <TextField
+          className="input-field"
+          type="number"
+          value={props.num}
+          onChange={handleChange}
+        />
+      </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default UserInput
+export default UserInput;
